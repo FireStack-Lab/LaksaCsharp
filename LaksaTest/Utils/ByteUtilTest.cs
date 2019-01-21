c7c1e7e529045427702c9d122dc2a3525c47ca90
yanbin007 <yanbin007@outlook.com>
2019-01-21 22:38:40 +0800

    unit test for ByteUtil

---
+++ LaksaTest/Utils/ByteUtilTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LaksaCsharp.Utils;
+
+namespace LaksaTest.Utils
+{
+    /// <summary>
+    /// ByteUtilTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class ByteUtilTest
+    {
+        [TestMethod]
+        public void HexStringToByteArray()
+        {
+            String hexString = "e19d05c5452598e24caad4a0d85a49146f7be089515c905ae6a19e8a578a6930";
+            String byteString = "225,157,5,197,69,37,152,226,76,170,212,160,216,90,73,20,111,123,224,137,81,92,144,90,230,161,158,138,87,138,105,48,";
+            byte[] bytes = ByteUtil.HexStringToByteArray(hexString);
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                stringBuilder.Append(b & 0xff);
+                stringBuilder.Append(",");
+            }
+            System.Console.WriteLine(stringBuilder.ToString());
+            Assert.AreEqual(stringBuilder.ToString(), byteString);
+        }
+    }
+}
