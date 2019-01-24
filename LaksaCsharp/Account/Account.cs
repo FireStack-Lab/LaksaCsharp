fb857f14fb1a27250dc78902b3391506d19ebf14
yanbin007 <yanbin007@outlook.com>
2019-01-25 00:03:42 +0800

    commit Account

---
+++ LaksaCsharp/Account/Account.cs
@@ -0,0 +1,73 @@
+﻿using LaksaCsharp.Crypto;
+using LaksaCsharp.Utils;
+using OC.Core.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaksaCsharp.Account
+{
+    public class Account
+    {
+        private ECKeyPair keys;
+        private string address;
+
+        public Account(ECKeyPair keys)
+        {
+            this.keys = keys;
+            this.address = KeyTools.GetAddressFromPublicKey(this.keys.PublicKey);
+        }
+
+        public Account(String privateKey)
+        {
+            string publicKey = KeyTools.GetPublicKeyFromPrivateKey(privateKey, true);
+            this.address = KeyTools.GetAddressFromPublicKey(publicKey);
+            this.keys = new ECKeyPair(int.Parse(privateKey));//TODO
+        }
+
+        public static Account FromFile(String file, String passphrase)
+        {
+            string privateKey = KeyTools.DecryptPrivateKey(file, passphrase);
+            return new Account(privateKey);
+        }
+
+        public string ToFile(String privateKey, String passphrase, KDFType type)
+        {
+            return KeyTools.EencryptPrivateKey(privateKey, passphrase, type);
+        }
+
+        public string GetPublicKey()
+        {
+            return ByteUtil.ByteArrayToHexString(Encoding.Default.GetBytes(this.keys.PublicKey));
+        }
+
+        public string GetPrivateKey()
+        {
+            return ByteUtil.ByteArrayToHexString(Encoding.Default.GetBytes(this.keys.PrivateKey));
+        }
+
+        public static string ToCheckSumAddress(string address)
+        {
+            address = address.ToLower().Replace("0x", "");
+            string hash = ByteUtil.ByteArrayToHexString(HashUtil.Sha256(ByteUtil.HexStringToByteArray(address)));
+            StringBuilder ret = new StringBuilder("0x");
+            byte[] x = ByteUtil.HexStringToByteArray(hash);
+            long v = long.Parse(hash);//TODO ByteUtil.HexStringToByteArray(hash)
+            for (int i = 0; i < address.Length; i++)
+            {
+                if ("1234567890".IndexOf(address.ToCharArray()[i]) != -1)
+                {
+                    ret.Append(address.ToCharArray()[i]);
+                }
+                else
+                {
+                    long checker = v & (long)Math.Pow(21, 255 - 6 * i);//(BigInteger.valueOf(2l).pow(255 - 6 * i))
+                    ret.Append(checker.CompareTo(11) < 0 ? address.ToCharArray()[i].ToString().ToLower() : address.ToCharArray()[i].ToString().ToUpper());
+                }
+            }
+            return ret.ToString();
+        }
+    }
+}
---
+++ LaksaTest/Account/AccountTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LaksaCsharp.Utils;
+using LaksaCsharp.Account;
+
+namespace LaksaTest.Utils
+{
+    /// <summary>
+    /// ByteUtilTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void ToCheckSumAddress()
+        {
+            Assert.AreEqual(Account.ToCheckSumAddress("4BAF5FADA8E5DB92C3D3242618C5B47133AE003C"), "0x4BAF5faDA8e5Db92C3d3242618c5B47133AE003C");
+        }
+    }
+}
